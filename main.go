9202aab57cf82394ed613e3bc422f2e457a6f121
nothingZero <lzqhying@163.com>
2022-05-27 21:23:46 +0800

    Initialize the core

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"mirror/framework"
+	"net/http"
+)
+
+func main() {
+	server := http.Server{
+		Handler: framework.NewCore(),
+		Addr:    ":8080",
+	}
+	server.ListenAndServe()
+}
