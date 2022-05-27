328b85111db60b0418cd9351edc1a1f45f4085bf
nothingZero <lzqhying@163.com>
2022-05-28 00:15:30 +0800

    Initialize the context

---
+++ controller.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"mirror/framework"
+	"time"
+)
+
+func FooControllerHandler(c *framework.Context) error {
+	finish := make(chan struct{}, 1)
+	panicChan := make(chan interface{}, 1)
+
+	durationCtx, cancel := context.WithTimeout(c.BaseContext(), time.Duration(1*time.Second))
+	// 这里记得当所有事情处理结束后调用 cancel，告知 durationCtx 的后续 Context 结束
+	defer cancel()
+
+	// mu := sync.Mutex{}
+	go func() {
+		defer func() {
+			if p := recover(); p != nil {
+				panicChan <- p
+			}
+		}()
+		// Do real action
+		time.Sleep(10 * time.Second)
+		c.Json(200, "ok")
+
+		finish <- struct{}{}
+	}()
+	select {
+	case p := <-panicChan:
+		c.WriterMux().Lock()
+		defer c.WriterMux().Unlock()
+		log.Println(p)
+		c.Json(500, "panic")
+	case <-finish:
+		fmt.Println("finish")
+	case <-durationCtx.Done():
+		c.WriterMux().Lock()
+		defer c.WriterMux().Unlock()
+		c.Json(500, "time out")
+		c.SetHasTimeout()
+	}
+	return nil
+}
